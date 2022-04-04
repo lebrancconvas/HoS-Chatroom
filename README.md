8eb70a067c6955deab719e8b84d7c9f427ad0e98
lebrancconvas <lebrancconvas@gmail.com>
2022-04-04 21:52:39 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hos-chatroom
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/quickstart-liff-babmyg)
---
+++ index.js
@@ -0,0 +1,32 @@
+// Import stylesheets
+import "./style.css"
+
+// Body element
+const body = document.getElementById('body')
+
+// Button elements
+const btnSend = document.getElementById("btnSend")
+const btnClose = document.getElementById("btnClose")
+const btnShare = document.getElementById("btnShare")
+const btnLogIn = document.getElementById("btnLogIn")
+const btnLogOut = document.getElementById("btnLogOut")
+const btnScanCode = document.getElementById("btnScanCode")
+const btnOpenWindow = document.getElementById("btnOpenWindow")
+
+// Profile elements
+const email = document.getElementById("email")
+const userId = document.getElementById("userId")
+const pictureUrl = document.getElementById("pictureUrl")
+const displayName = document.getElementById("displayName")
+const statusMessage = document.getElementById("statusMessage")
+
+// QR element
+const code = document.getElementById("code")
+const friendShip = document.getElementById("friendShip")
+
+async function main() {
+  // Initialize LIFF app)
+  
+  // Try a LIFF function
+}
+main()
